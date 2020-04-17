82eb60335eacf4b03b827adab875a449a1f4bf13
Adri Forczek <forczekadri@gmail.com>
2020-04-17 12:24:38 +0100

    chore: calculation of squareroot without py libraries

--- 01_squareroot.py
+++ 01_squareroot.py
@@ -4,11 +4,39 @@
 # The expected time complexity is O(log(n))
 
 def sqrt(number):
-   num = number*(1/2)
-   print(num)
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+    if number < 0:
+        return None
+
+    if (number == 0) or (number == 1):
+        return number
+
+    else:
+        result = number ** 0.5
+        # returns floor value
+        return int(result // 1)
+
+
+print("basic tests")
+print("Pass" if  (3 == sqrt(9)) else "Fail") # Pass
+print("Pass" if  (0 == sqrt(0)) else "Fail") # Pass
+print("Pass" if  (1 == sqrt(1)) else "Fail") # Pass
+print("Pass" if  (5 == sqrt(27)) else "Fail") # Pass
+print("Pass" if (None == sqrt(-1)) else "Fail") # Pass
+
+print("------")
+print("squares of 3:")
+print("Pass" if  (3 == sqrt(10)) else "Fail") # Pass
+print("Pass" if  (3 == sqrt(11)) else "Fail") # Pass
+print("Pass" if  (3 == sqrt(12)) else "Fail") # Pass
+print("Pass" if  (3 == sqrt(13)) else "Fail") # Pass
+print("Pass" if  (3 == sqrt(14)) else "Fail") # Pass
+print("Pass" if  (3 == sqrt(15)) else "Fail") # Pass
+print("Pass" if  (3 == sqrt(16)) else "Fail") # Fail
+print("Pass" if  (4 == sqrt(16)) else "Fail") # Pass
+
+print("------")
+print("large numbers")
+print("Pass" if (12345 == sqrt(152399025)) else "Fail") # Pass
+print("Pass" if (578 == sqrt(334084)) else "Fail") # Pass
+print("Pass" if (None == sqrt(-334084)) else "Fail") # Pass
