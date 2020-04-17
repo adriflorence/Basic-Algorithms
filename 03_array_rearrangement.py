a0b1fe7f9f7f5ba0174f407f87f8ca70949d2453
Adri Forczek <forczekadri@gmail.com>
2020-04-17 19:22:33 +0100

    chore: implements merge sort

--- 03_array_rearrangement.py
+++ 03_array_rearrangement.py
@@ -1,13 +1,46 @@
-def rearrange_digits(input_list):
-    """
-    Rearrange Array Elements so as to form two number such that their sum is maximum.
+# Merge Sort is a Divide and Conquer algorithm.
+# It divides input array in two halves, calls itself for the two halves and then merges the two sorted halves.
 
-    Args:
-       input_list(list): Input List
-    Returns:
-       (int),(int): Two maximum sums
-    """
-    pass
+
+# implements merge sort to sort array
+def rearrange_digits(input_list):
+    if len(input_list) <= 1:
+        return input_list
+
+    middle = len(input_list) // 2
+    left = rearrange_digits(input_list[:middle])
+    right = rearrange_digits(input_list[middle:])
+
+    return compare(left, right)
+
+def compare(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+
+    # iterate and compare left to right
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    
+    # add any remaining items from the left array
+    while left_index < len(left):
+        result.append(left[left_index])
+        left_index += 1
+
+    # add any remaining items form the right array
+    while right_index < len(right):
+        result.append(right[right_index])
+        right_index += 1
+
+    # merged and sorted array
+    return result
+
+# TEST
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -17,5 +50,7 @@ def test_function(test_case):
     else:
         print("Fail")
 
-test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+print(rearrange_digits([4, 6, 2, 5, 9, 8]))
+
+# test_function([[1, 2, 3, 4, 5], [542, 31]])
+# test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
